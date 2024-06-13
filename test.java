73d0f5f6c55faacfef8d4a79acbc625b28bab280
edwinljila <eljila@melontech.com>
2024-06-13 10:15:18 +0200

    Architect develop

--- test.java
+++ test.java
@@ -5,7 +5,11 @@ public class test {
     @Test
     public void test()
     {
-        
+        System.out.println("This is for git purposes only, testing git branching");
     }
-
+    @Test
+    public void test2()
+    {
+        System.out.println("This is for git purposes only, testing git branching second test");
+    }
 }
